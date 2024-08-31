8037e9ea996bd1705a7c401ca2e04517f5a9e242
youshimoto <132978829+youshimoto@users.noreply.github.com>
2024-08-31 20:23:56 +0800

    Grp5_Medina_ex2.py

---
+++ Grp5_Medina_ex2.py
@@ -0,0 +1,65 @@
+#1 
+num1 = 19  
+num2 = 5
+
+result_add = num1 + num2
+result_sub = num1 - num2
+result_mul = num1 * num2
+result_div = num1 / num2
+result_mod = num1 % num2
+result_exp = num1 ** num2
+
+print("Addition: ", result_add)
+print("Subtraction: ", result_sub)
+print("Multiplication: ", result_mul)
+print("Division: ", result_div)
+print("Modulus: ", result_mod)
+print("Exponent: ", result_exp)
+
+num1 = 20.5
+num2 = 7.5
+
+result_add = num1 + num2
+result_sub = num1 - num2
+result_mul = num1 * num2
+result_div = num1 / num2
+result_mod = num1 % num2
+result_exp = num1 ** num2
+
+print("\nAddition (float): ", result_add)
+print("Subtraction (float): ", result_sub)
+print("Multiplication (float): ", result_mul)
+print("Division (float): ", result_div)
+print("Modulus (float): ", result_mod)
+print("Exponent (float): ", result_exp)
+
+num1 = 3 + 4
+num2 = 2 + 5
+result_add = num1 + num2
+result_sub = num1 - num2
+result_mul = num1 * num2
+result_div = num1 / num2
+result_mod = abs(num1 - num2)  
+result_exp = num1 ** 0.5  
+print("\nAddition (complex): ", result_add)
+print("Subtraction (complex): ", result_sub)
+print("Multiplication (complex): ", result_mul)
+print("Division (complex): ", result_div)
+print("Modulus (complex): ", result_mod)
+print("Exponent (complex): ", result_exp)
+
+#2 
+num1 = 25_000_000 
+num2 = 25000000    
+
+print(num1)
+print(num2)
+
+#3 
+int_var = 19
+float_var = 19.0
+complex_var = 19 + 0
+
+print(f"Type of int_var: {type(int_var)}")
+print(f"Type of float_var: {type(float_var)}")
+print(f"Type of complex_var: {type(complex_var)}")
